3fb8d06c0c3d88dea992a04cd23d3c9f77e31d4b
nnarct <nannapat1551@gmail.com>
2023-09-19 22:57:59 +0700

    edit: edit some texts & variable size

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[40], G[5], answer[10];
+{ char name[80], G[5], answer[10];
   int i;
   do
   {
-    printf("What is your NAME? ");
-    scanf("%s", Name);
+    printf("vad heter du? ");
+    scanf("%s", name);
     do
     {
       printf("What is your blood group [A, B, AB, or O]? ");
@@ -23,26 +23,26 @@ void main()
                             strcmp(G, "O") !=0 );
     if (strcmp(G,"A") ==0)
     {
-      printf("%s, A. Hey, you can give blood to: A, AB.\n", Name);
+      printf("%s, A. Hey, you can give blood to: A, AB.\n", name);
       printf("  You can receive blood from: A, O.\n");
     }
     else if (strcmp(G, "B") ==0)
     {
-      printf("%s, B. Well, you can give blood to: B, AB.\n", Name);
+      printf("%s, B. Well, you can give blood to: B, AB.\n", name);
       printf("  You can receive blood from: B, O.\n");
     }
     else if (strcmp(G, "AB") == 0)
     {
-      printf("%s, AB. Oh my God, you can give blood only to: AB.\n", Name);
+      printf("%s, AB. Oh my God, you can give blood only to: AB.\n", name);
       printf("  Wow, you can receive blood from all: O, A, B, AB.\n");
     }
     else
     {
-      printf("%s, O. Nice! You can give blood to all: O, A, B, AB\n", Name);
+      printf("%s, O. Nice! You can give blood to all: O, A, B, AB\n", name);
       printf("  But Sad! You can receive blood only from: O\n");
     }
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YESsss") == 0);
-  printf("Goodbye\n");
+  printf("Adjö\n");
 }
